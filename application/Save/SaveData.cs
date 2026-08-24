5ae2d084a2940327e798d265bbfe658d81721008
Eder Rodriguez <eo.rodriguezcastillo@gmail.com>
2026-08-23 23:14:04 -0600

    fix: make save collections serializable

--- application/Save/SaveData.cs
+++ application/Save/SaveData.cs
@@ -6,6 +6,6 @@ public sealed record SaveData
     public double MusicVolume { get; init; } = 1;
     public double SfxVolume { get; init; } = 1;
     public int HighestUnlockedLevel { get; init; } = 1;
-    public IReadOnlySet<string> CompletedTutorialSteps { get; init; } = new HashSet<string>(StringComparer.Ordinal);
-    public IReadOnlyDictionary<string, int> DisabledBoosterInventory { get; init; } = new Dictionary<string, int>(StringComparer.Ordinal);
+    public HashSet<string> CompletedTutorialSteps { get; init; } = new(StringComparer.Ordinal);
+    public Dictionary<string, int> DisabledBoosterInventory { get; init; } = new(StringComparer.Ordinal);
 }
